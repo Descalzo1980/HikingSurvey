e9c0b2bdca42fe2f79256cc13efbd179869f6247
Stas <ragioniere1980@gmail.com>
2025-07-10 00:32:00 +0300

    add text
    add text

--- HikingSurvey/ContentView.swift
+++ HikingSurvey/ContentView.swift
@@ -1,19 +1,25 @@
-//
-//  ContentView.swift
-//  HikingSurvey
-//
-//  Created by Станислав Леонов on 10.07.2025.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var responses: [Response] = []
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Opinions on Hiking")
+                .frame(maxWidth: .infinity)
+                .font(.title)
+                .padding(.top, 24)
+            ScrollView {
+                ForEach(responses) {response in
+                    Text(response.text)
+                }
+            }
+        }
+        .onAppear {
+            for response in Response.sampleResponses {
+                responses.insert(Response(text: response),at: 0)
+            }
         }
         .padding()
     }
