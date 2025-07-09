a599d2f598b286e07367362df18ad424ae2a4679
Stas <ragioniere1980@gmail.com>
2025-07-10 00:12:02 +0300

    first commit

---
+++ HikingSurvey/HikingSurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingSurveyApp.swift
+//  HikingSurvey
+//
+//  Created by Станислав Леонов on 10.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct HikingSurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
