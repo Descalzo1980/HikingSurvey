5acb31099ba5b603658173a0ccf39706c9fb0133
Stas <ragioniere1980@gmail.com>
2025-07-11 13:06:46 +0300

    add rw
    add rw

---
+++ HikingSurvey/UI/ResponseView.swift
@@ -0,0 +1,19 @@
+
+import SwiftUI
+
+struct ResponseView: View {
+    var response: Response
+    
+    var body: some View {
+        Text(response.text)
+            .frame(maxWidth: .infinity,alignment: .leading)
+            .padding(16)
+            .background(RoundedRectangle(cornerRadius: 8)
+                .fill(.white)
+            )
+    }
+}
+
+#Preview {
+    ResponseView(response: Response(text: "I love this song"))
+}
