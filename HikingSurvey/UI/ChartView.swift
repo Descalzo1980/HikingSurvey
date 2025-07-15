2433cb3582850ed0660b3db4ab460e163cd28476
Stas <ragioniere1980@gmail.com>
2025-07-15 12:11:06 +0300

    add animation
    add animation

--- HikingSurvey/UI/ChartView.swift
+++ HikingSurvey/UI/ChartView.swift
@@ -1,23 +1,17 @@
-//
-//  ChartView.swift
-//  HikingSurvey
-//
-//  Created by Станислав Леонов on 14.07.2025.
-//
-
 import SwiftUI
 import Charts
 
 struct ChartView: View {
     var response: [Response]
-    
+    @State private var flipped = false
+    @State private var fadedOut = false
     init(response: [Response]) {
         self.response = response.sorted { $0.score < $1.score }
     }
     
     var body: some View {
         Chart(response) { response in
-            SectorMark(angle: .value("Type", 1))
+            SectorMark(angle: .value("Type", 1),innerRadius: .ratio(0.75))
                 .foregroundStyle(by: .value("sentiment",response.sentiment))
         }
         .chartForegroundStyleScale([
@@ -25,6 +19,33 @@ struct ChartView: View {
             Sentiments.negative: Sentiments.negative.sentimentColor,
             Sentiments.moderate: Sentiments.moderate.sentimentColor
         ])
+        .chartBackground { chartProxy in
+            GeometryReader { geomerty in
+                if let anchor = chartProxy.plotFrame {
+                    let frame = geomerty[anchor]
+                    Image(systemName: "figure.hiking")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: frame.height * 0.4)
+                        .foregroundStyle(Color(white:0.59))
+                        .position(x: frame.midX, y: frame.midY)
+                        .scaleEffect(x: flipped ? -1 : 1, y: 1)
+                        .opacity(fadedOut ? 0 : 1)
+                        .position(x: frame.midX, y: frame.midY)
+                        .animation(.easeInOut(duration: 1), value: flipped)
+                        .animation(.easeInOut(duration: 1), value: fadedOut)
+                        .onAppear {
+                            Timer.scheduledTimer(withTimeInterval: 3.0, repeats: true) { _ in
+                                fadedOut = true
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                                    flipped.toggle()
+                                    fadedOut = false
+                                }
+                            }
+                        }
+                }
+            }
+        }
         .chartLegend(.hidden)
         .frame(height: 200)
         .padding()
